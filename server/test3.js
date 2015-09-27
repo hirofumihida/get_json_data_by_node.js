904b5872e669e97691aed9af8a3e9661f8900b04
Hirofumi Hida <hirofumi.hida@gmail.com>
2015-09-27 22:14:00 +0900

    add server files

---
+++ server/test3.js
@@ -0,0 +1,22 @@
+var fs   = require('fs');
+var content = null;
+var filepath = './test.json';
+
+function reload() {
+    content = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    return content;
+}
+
+var xmlrpc = require('xmlrpc');
+
+var server = xmlrpc.createServer({ host: '0.0.0.0', port: 9090 })
+server.on('NotFound', function(method, params) {
+  console.log('Method ' + method + ' does not exist');
+})
+
+server.on('get', function (err, params, callback) {
+  console.log('Method call params for \'get\': ' + params)
+  data = reload()
+  callback(null, content[0])
+})
+console.log('XML-RPC server listening on port 9090')
